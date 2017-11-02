8d6f0f7aa991369d96e52cae4658e21726227cd1
Исламов Айдар Альбертович <lowgear1000@gmail.com>
2017-11-02 19:49:42 +0500

    нормальная версия

--- HomeExercises/ObjectComparison.cs
+++ HomeExercises/ObjectComparison.cs
@@ -15,10 +15,9 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			
-		    actualTsar.ShouldBeEquivalentTo(expectedTsar, options => options
-                .Excluding(o => o.Id)
-                .Excluding(o => o.Parent.Id));
+
+			actualTsar.ShouldBeEquivalentTo(expectedTsar, options => options
+				.Excluding(o => o.SelectedMemberInfo.Name == nameof(Person.Id)));
 		}
 
 		[Test]
