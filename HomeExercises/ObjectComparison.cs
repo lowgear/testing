378c508fd6bc4d1d07665c595973844207308250
Исламов Айдар Альбертович <lowgear1000@gmail.com>
2017-11-02 19:35:56 +0500

    blabla

--- HomeExercises/ObjectComparison.cs
+++ HomeExercises/ObjectComparison.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using FluentAssertions;
-using FluentAssertions.Equivalency;
 using NUnit.Framework;
 
 namespace HomeExercises
